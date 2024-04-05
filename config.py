985d68806215ab80ddd9c9e7569c98de63236928
file2bots <166150891+file2bots@users.noreply.github.com>
2024-04-05 16:19:00 -0400

    Update config.py

--- config.py
+++ config.py
@@ -92,7 +92,7 @@ class Var(object):
     else:
         ON_HEROKU = False
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-    HAS_SSL=bool(getenv('HAS_SSL',True))
+    HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
         URL = "https://familiar-miguelita-filex2bots.koyeb.app/"
     else:
